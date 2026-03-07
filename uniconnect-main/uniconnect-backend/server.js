8f0b49e34cf7fb811c47e9658d06e7e727d35d10
mitaliraj3007 <rajmitali1604@gmail.com>
2026-03-07 23:19:30 +0530

    env file added

--- uniconnect-main/uniconnect-backend/server.js
+++ uniconnect-main/uniconnect-backend/server.js
@@ -4,6 +4,7 @@ import itemRoutes from "./routes/ItemRoutes.js";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import mongoose from "mongoose";
 import connectDB from "./config/db.js";
 import postRoutes from "./routes/postRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -25,3 +26,7 @@ app.use("/api/events", eventRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ MongoDB connection error:", err));
